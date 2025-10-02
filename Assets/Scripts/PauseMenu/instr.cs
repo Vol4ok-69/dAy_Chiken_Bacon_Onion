da8f1b9f8c4c321b98ac1041346ea87fc57c7432
Abdulaziz Mirzakamilov <alikom904@gmail.com>
2025-10-03 00:54:21 +0700

    Pause Menu work

---
+++ Assets/Scripts/PauseMenu/instr.cs
@@ -0,0 +1,64 @@
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class instr : MonoBehaviour
+{
+    public GameObject tutorialPanel;
+    public GameObject Panel;
+    public GameObject ConfirmPanel;
+    private bool isPaused = false;
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        
+    }
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Escape))
+        {
+            if (isPaused)
+                ResumeGame();
+            else
+                PauseGame();
+        }
+    }
+    public void PauseGame()
+    {
+        Panel.SetActive(true);
+        Time.timeScale = 0f;
+        isPaused = true;
+    }
+    public void ResumeGame()
+    {
+        Panel.SetActive(false); 
+        Time.timeScale = 1f;
+        tutorialPanel.SetActive(false);
+        isPaused = false;
+    }
+    public void OpenTutorial()
+    {
+        tutorialPanel.SetActive(true);
+        isPaused = true;
+    }
+    public void CloseTutorial()
+    {
+        tutorialPanel.SetActive(false);
+        isPaused = false;
+    }
+
+    public void OpenConfirmPanel()
+    {
+        ConfirmPanel.SetActive(true);
+    }
+
+    public void CloseConfirmPanel()
+    {
+        ConfirmPanel.SetActive(false);
+    }
+
+    public void GoToMainMenu()
+    {
+        Time.timeScale = 1f;
+        SceneManager.LoadScene("MainMenu");
+    }
+}
